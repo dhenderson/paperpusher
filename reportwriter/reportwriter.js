4aeaa1dd6537b1aac9308b3862ca3f74527c5840
David Henderson <dhenderson@idealistics.org>
2013-07-19 18:53:55 -0700

    Started transform variable options, doesn't save yet.

--- reportwriter/reportwriter.js
+++ reportwriter/reportwriter.js
@@ -22,7 +22,7 @@ function save_report_json_file(json_data, report_file_name){
 	update_variables();
 	
 	/** convert the report_data into a json string and download the file**/
-	json_string = JSON.stringify(json_data);
+	json_string = JSON.stringify(json_data, null, '\t');
 	report_file_name = report_file_name + ".json";
 	var blob = new Blob([json_string], {type: "text/plain;charset=utf-8"});
 	saveAs(blob, report_file_name);
@@ -50,8 +50,25 @@ function update_variables(){
 		var variable = variables[i];
 		
 		var variable_id = variable.id;
+		
+		/** name **/
 		var variable_name = document.getElementById(variable_id + "_name").innerHTML;
-		report_data["variables"][variable_name] = {}
+		report_data["variables"][variable_name] = {};
+		
+		/** data type **/
+		var data_type = document.getElementById(variable_id + "_data_type");
+		data_type_value = data_type.options[data_type.selectedIndex].text;
+		report_data["variables"][variable_name]["data_type"] = data_type_value;
+		
+		/** is_transform **/
+		var is_transform = document.getElementById(variable_id + "_is_transform");
+		is_transform_value = is_transform.options[is_transform.selectedIndex].text;
+		
+		var is_transform_bool = false;
+		if (is_transform_value=="true") {
+			is_transform_bool = true;
+		}
+		report_data["variables"][variable_name]["is_transform"] = is_transform_bool;
 	}
 }
 
@@ -83,7 +100,7 @@ function add_variables_by_string(variable_names_str) {
 function add_variable(variable_name){
 	var variables_container = document.getElementById('variables');
 	
-	var variable_id = 'variable_' + variable_name.replace(/\W/g, '');
+	var variable_id = variable_name.replace(/\W/g);
 	
 	/**Create a new container for this variable**/
 	var variable = document.createElement('div');
@@ -91,20 +108,141 @@ function add_variable(variable_name){
 	variable.setAttribute('id', variable_id)
 	
 	/** variable name **/
-	var variable_name_div = document.createElement('div');
-	variable_name_div.setAttribute('id', variable_id + "_name")
-	variable_name_div.innerHTML = variable_name;
-	variable.appendChild(variable_name_div)
+	var variable_name_display = document.createElement('h2');
+	variable_name_display.setAttribute('id', variable_id + "_name")
+	variable_name_display.innerHTML = variable_name;
+	variable.appendChild(variable_name_display)
+	
+	/** data type **/
+	var data_type_label = document.createElement('label')
+	data_type_label.innerHTML = "Data type";
+	variable.appendChild(data_type_label);
+	
+	var data_type = document.createElement('select');
+	data_type.setAttribute('id', variable_id + "_data_type")
+
+		/** int **/
+		data_type_int = document.createElement('option');
+		data_type_int.innerHTML = "Integer";
+		data_type.appendChild(data_type_int);
+		
+		/** float **/
+		data_type_float = document.createElement('option');
+		data_type_float.innerHTML = "Float";
+		data_type.appendChild(data_type_float);
+		
+		/** date **/
+		data_type_date = document.createElement('option');
+		data_type_date.innerHTML = "Date";
+		data_type.appendChild(data_type_date);
+		
+		/** boolean **/
+		data_type_boolean = document.createElement('option');
+		data_type_boolean.innerHTML = "Boolean";
+		data_type.appendChild(data_type_boolean);
+		
+		/** string **/
+		data_type_string = document.createElement('option');
+		data_type_string.innerHTML = "String";
+		data_type.appendChild(data_type_string);
+	
+	variable.appendChild(data_type);
+	
+	/** is transform **/
+	
+	var is_transform_label = document.createElement('label')
+	is_transform_label.innerHTML = "Transform this variable";
+	variable.appendChild(is_transform_label);
+	
+	var is_transform = document.createElement('select');
+	is_transform.setAttribute('id', variable_id + "_is_transform")
+	is_transform.setAttribute('onchange', 'toggle_transform_options(this.options[this.selectedIndex].value,"' + variable_id + '")');
+
+		/** false **/
+		is_transform_false = document.createElement('option');
+		is_transform_false.innerHTML = "false";
+		is_transform.appendChild(is_transform_false);
+		
+		/** true **/
+		is_transform_true = document.createElement('option');
+		is_transform_true.innerHTML = "true";
+		is_transform.appendChild(is_transform_true);
+		
+	variable.appendChild(is_transform);
 	
 	/** remove variable **/
-	var remove_variable = document.createElement('span')
-	remove_variable.setAttribute('onclick','remove_variable("' + variable_id + '")')
-	remove_variable.setAttribute('class','clickable')
-	remove_variable.innerHTML = " [Remove] "
-	variable.appendChild(remove_variable)
+	var remove_variable = document.createElement('div');
+	remove_variable.setAttribute('onclick','remove_variable("' + variable_id + '")');
+	remove_variable.setAttribute('class','clickable');
+	remove_variable.innerHTML = "[Remove]";
+	variable.appendChild(remove_variable);
 	
 	/** Add the variable to the variables container **/
-	variables_container.appendChild(variable)
+	variables_container.appendChild(variable);
+}
+
+function toggle_transform_options(is_transform, variable_id){
+
+	/** variable dom element **/
+	var variable = document.getElementById(variable_id);
+	
+	if (is_transform == "true"){
+		set_transform_options(variable_id)
+	}
+	else{
+		var transform_options_id = variable_id + "_transform_options";
+		var transform_options = document.getElementById(transform_options_id);
+		variable.removeChild(transform_options);
+	}
+}
+
+function set_transform_options(variable_id){
+	/** update the variables before starting **/
+	update_variables();
+	
+	/** variable dom element **/
+	var variable = document.getElementById(variable_id);
+	/** variable options registered with the report_data **/
+	var report_variables = report_data["variables"];
+	
+	/** transform options **/
+	transform_options = document.createElement('div');
+	transform_options.setAttribute('id', variable_id + "_transform_options");
+	transform_options.setAttribute('class', "transform_options");
+	variable.appendChild(transform_options);
+	
+	/** variable container **/
+	variable_checkbox_container = document.createElement('div');
+	transform_options.appendChild(variable_checkbox_container);
+	
+	for (var report_variable_name in report_variables) {
+		var report_variable = report_variables[report_variable_name];
+		var is_transform = report_variable['is_transform'];
+		if(!is_transform){
+			/**non-transformed variable, so add it as a selectable option**/
+			
+			
+			variable_checkbox_name = document.createElement('span');
+			variable_checkbox_name.innerHTML = report_variable_name;
+			
+			variable_checkbox_container.appendChild(variable_checkbox_name);
+			
+			variable_checkbox = document.createElement('input');
+			variable_checkbox.setAttribute('type','checkbox');
+			variable_checkbox.setAttribute('value','checkbox');
+			variable_checkbox.checked = false;
+			variable_checkbox_container.appendChild(variable_checkbox);
+			
+			/**TODO: check if this option has been selected already**/
+		}
+	}
+	
+	/** the method to use **/
+	//var method_label = document.createElement('label')
+	//method.innerHTML = "Method (not implemented)";
+	//variable.appendChild(method_label);
+
+	
 }
 
 /**
